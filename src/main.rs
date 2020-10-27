f1f8e0c828af10992fbb215aca0c1cfb251bc517
Antash <mishraantash34@gmail.com>
2020-10-27 10:54:06 +0530

    Error handlers

--- src/main.rs
+++ src/main.rs
@@ -3,10 +3,15 @@
 #[macro_use]extern crate rocket;
 #[macro_use] extern crate rocket_contrib;
 #[macro_use] extern crate serde_derive;
+extern crate r2d2_sqlite;
+extern crate r2d2;
 extern crate env_logger;
 pub extern crate rusqlite;
 extern crate serde_json;
+#[macro_use]
+extern crate lazy_static;
 
+use r2d2::{Pool, PooledConnection};
 use rocket_contrib::databases;
 use rusqlite::{Connection, MappedRows, Result, Row, types::FromSql, types::ToSql};
 use std::fs::File;
@@ -15,11 +20,20 @@ use serde::{Serialize, Deserialize};
 use rocket::{Rocket, State, response::{Debug, content}, routes};
 use std::sync::Mutex;
 use rocket_contrib::json::Json;
+use rocket_contrib::serve::StaticFiles;
 use std::marker::Sync;
+use rocket::fairing::AdHoc;
+use std::sync::RwLock;
 
 
-#[database("sqlite_logs")]
-struct LogsDbConn(rusqlite::Connection);
+#[database("SqliteDbConn")] 
+struct SqliteDbConn(rusqlite::Connection);
+
+impl SqliteDbConn {
+    pub fn connection(&mut self) -> &mut rusqlite::Connection {
+        &mut self.0
+    }
+}
 
 #[derive(Debug,Serialize, Deserialize)]
 struct Movies {
@@ -54,31 +68,43 @@ fn read_sql_from_file(path: &str) -> String {
     contents
 }
 
-#[get("/movies")]
-fn get_movies(conn: LogsDbConn) -> Json<Datas> {
+fn create_db(conn: &Connection, sql_content: String) -> Result<()> {
+    conn.execute(   
+        sql_content.as_str(), &[]
+    ).expect("No table found");
 
-    let mut stmt = conn.prepare("SELECT * FROM MOVIES").unwrap();
-    let movies_iter =  stmt.query_map( &[], |row| {
-        Ok(Movies {
-            movie_id:  row.get(0),
-            title: row.get(1),
-            genre: row.get(2),
-            imdb_rating: row.get(3)
-        })
-    }).unwrap();
+    Ok(())
 
-    let mut db_datas = Vec::new();
+}
 
-    for data in movies_iter {
-        db_datas.push(data.unwrap());
-    }
+fn my_movies(conn: &Connection) -> Result<Json<Datas>> {
+    let mut stmt = conn.prepare("SELECT * FROM Movies").expect("Movies not found");
+    let mut all_movies =  stmt
+        .query_map( &[], |row| {
+            Movies {
+                movie_id:  row.get(0),
+                title: row.get(1),
+                genre: row.get(2),
+                imdb_rating: row.get(3)
+            }
+        }).unwrap()
+        .into_iter()
+        .collect::<Result<Vec<Movies>>>()?; 
+
+        println!("{:?}", all_movies);
 
 
-
-    Json(Datas {
-        all_movies: db_datas,
-    })
+    Ok(Json(Datas {
+        all_movies,
+    }))
     
+}
+
+#[get("/movies")]
+fn get_movies(conn: SqliteDbConn) -> Result<Json<Datas>> {
+    println!("hey");
+
+    my_movies(&*conn)
     //let movies_json = serde_json::to_string(&movies_iter).unwrap();
     //println!("{}", movies_json);        
 }
@@ -87,21 +113,19 @@ fn main() {
 
     env_logger::init();
 
-    
     let db_conn = Connection::open("file.db").unwrap();
 
 
-
-    let sql_file_content = read_sql_from_file("all.sql"); 
+    let mut sql_file_content = read_sql_from_file("all.sql"); 
     println!("{}", sql_file_content);
-    db_conn.execute(   
-        sql_file_content.as_str(), &[]
-    ).expect("No table found");
+        
+    create_db(&db_conn, sql_file_content).unwrap();
+    println!("hello");
 
     rocket::ignite()
-        .attach(LogsDbConn::fairing())
+        .attach(SqliteDbConn::fairing())
+        .mount("/", routes![get_movies])
         //.manage(Mutex::new(db_conn))
-        .mount("/api", routes![get_movies])
-        .launch();
+        .launch();  
     
 }
